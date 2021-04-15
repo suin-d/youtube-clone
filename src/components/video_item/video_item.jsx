6790e496df20f58d0f7d628de22dff8a02dd3477
suin-d <everytheone@gmail.com>
2021-04-15 15:15:25 +0900

    Add VideoDetail component

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import styles from './video_item.module.css';
 
-export default function VideoItem({ video: { snippet } }) {
+export default function VideoItem({ video, onVideoClick, display }) {
+  const displayType = display === 'list' ? styles.list : styles.grid;
   return (
-    <li className={styles.container}>
+    <li
+      className={`${styles.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+    >
       <div className={styles.video}>
         <img
           className={styles.thumbnail}
-          src={snippet.thumbnails.medium.url}
+          src={video.snippet.thumbnails.medium.url}
           alt="video thumbnail"
         />
         <div className={styles.metadata}>
-          <p className={styles.title}>{snippet.title}</p>
-          <p className={styles.channel}>{snippet.channelTitle}</p>
+          <p className={styles.title}>{video.snippet.title}</p>
+          <p className={styles.channel}>{video.snippet.channelTitle}</p>
         </div>
       </div>
     </li>
